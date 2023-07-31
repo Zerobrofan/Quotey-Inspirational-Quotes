1fea15e88c85ad9c4fe5975292126a60fa0cf151
Abdallah Amir <abdallahamirmonir@gmail.com>
2023-07-31 16:14:05 +0300

    Add files via upload

--- extension/popup.js
+++ extension/popup.js
@@ -7,12 +7,12 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 });
 
-
 async function getQuote() {
     try {
+        // First API
         const response = await fetch("https://api.quotable.io/random");
         const data = await response.json();
-        
+
         const quoteElement = document.getElementById('quote');
         const authorElement = document.getElementById('author');
 
@@ -23,6 +23,36 @@ async function getQuote() {
             authorElement.innerHTML = author;
         }
     } catch (error) {
-        console.error(error);
+        console.error("First API error:", error);
+        // If the first API fails, try the second API as a fallback
+        try {
+            const category = 'inspirational';
+            const apiKey = 'YOUR-API-KEY';
+            const response = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+                headers: {
+                    'X-Api-Key': apiKey
+                }
+            });
+            const data = await response.json();
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const quoteElement = document.getElementById('quote');
+            const authorElement = document.getElementById('author');
+
+            if (quoteElement && authorElement) {
+                const quote = data[randomIndex].quote;
+                const author = "~ " + data[randomIndex].author || "~ null";
+                quoteElement.innerHTML = quote;
+                authorElement.innerHTML = author;
+            }
+        } catch (error) {
+            console.error("Second API error:", error);
+            // Display an error message if both APIs fail
+            const quoteElement = document.getElementById('quote');
+            const authorElement = document.getElementById('author');
+            if (quoteElement && authorElement) {
+                quoteElement.innerHTML = "Failed to fetch a quote.";
+                authorElement.innerHTML = "~ null";
+            }
+        }
     }
 }
